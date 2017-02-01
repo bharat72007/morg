f61963f447491865c4e60f4cd42697f51ffe7761
bharat <bharat72007@gmail.com>
2017-02-01 23:05:54 +0530

    attaching struct to create criteria

--- query.go
+++ query.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 )
 
 type Query struct {
 	Projection string
 	Filter     *Criteria
-	From       string
+	/*From       string*/
 }
 
 type Criteria struct {
 	//can have list of Restriction
 	Restrictions []interface{}
+	combiners    []string
+	Entity       interface{}
 }
 
 type Restriction struct {
@@ -35,8 +38,8 @@ type InRestriction struct {
 	resttype string
 }
 
-func CreateCriteria() *Criteria {
-	return &Criteria{Restrictions: make([]interface{}, 0)}
+func CreateCriteria(class interface{}) *Criteria {
+	return &Criteria{Restrictions: make([]interface{}, 0), combiners: make([]string, 0), Entity: class}
 }
 
 func NewBetweenRestriction(col string, lval, rval interface{}, rtype string) *BetweenRestriction {
@@ -57,27 +60,37 @@ func NewQuery() *Query {
 
 func (criteria *Criteria) add(restriction interface{}) *Criteria {
 	criteria.Restrictions = append(criteria.Restrictions, restriction)
+	if len(criteria.Restrictions) > 1 {
+		criteria.combiners = append(criteria.combiners, "AND")
+	}
 	return criteria
 }
 
-/*func (criteria *Criteria) addOr(restriction1, restriction2 *Restriction) *Criteria {
-	criteria.Restrictions = append(criteria.Restrictions, *restriction1)
-	criteria.Restrictions = append(criteria.Restrictions, *restriction2)
+func (criteria *Criteria) exclude(restriction interface{}) *Criteria {
+	criteria.Restrictions = append(criteria.Restrictions, restriction)
+	criteria.combiners = append(criteria.combiners, "NOT")
 	return criteria
-}*/
+}
 
-func (restriction Restriction) tostring() string {
+func (criteria *Criteria) addOr(restriction1, restriction2 interface{}) *Criteria {
+	criteria.Restrictions = append(criteria.Restrictions, restriction1)
+	criteria.Restrictions = append(criteria.Restrictions, restriction2)
+	criteria.combiners = append(criteria.combiners, "OR")
+	return criteria
+}
+
+func (restriction *Restriction) tostring() string {
 
 	strs := restriction.column + " " + restriction.resttype + " " + restriction.value.(string)
 	return strs
 }
 
-func (restriction BetweenRestriction) tostring() string {
+func (restriction *BetweenRestriction) tostring() string {
 	strs := "BETWEEN" + " " + restriction.column + " " + restriction.lbound.(string) + " " + "AND" + " " + restriction.ubound.(string)
 	return strs
 }
 
-func (restriction InRestriction) tostring() string {
+func (restriction *InRestriction) tostring() string {
 	newlist := make([]string, 0)
 	for _, v := range restriction.list {
 		newlist = append(newlist, v.(string))
@@ -86,39 +99,39 @@ func (restriction InRestriction) tostring() string {
 	return strs
 }
 
-func (restriction Restriction) Equal(col string, val interface{}) *Restriction {
+func (restriction *Restriction) Equal(col string, val interface{}) *Restriction {
 	return NewRestriction(col, val, "=")
 }
 
-func (restriction Restriction) NotEqual(col string, val interface{}) *Restriction {
+func (restriction *Restriction) NotEqual(col string, val interface{}) *Restriction {
 	return NewRestriction(col, val, "<>")
 }
 
-func (restriction Restriction) Gt(col string, val interface{}) *Restriction {
+func (restriction *Restriction) Gt(col string, val interface{}) *Restriction {
 	return NewRestriction(col, val, ">")
 }
 
-func (restriction Restriction) Gte(col string, val interface{}) *Restriction {
+func (restriction *Restriction) Gte(col string, val interface{}) *Restriction {
 	return NewRestriction(col, val, ">=")
 }
 
-func (restriction Restriction) Lte(col string, val interface{}) *Restriction {
+func (restriction *Restriction) Lte(col string, val interface{}) *Restriction {
 	return NewRestriction(col, val, "<=")
 }
 
-func (restriction Restriction) Lt(col string, val interface{}) *Restriction {
+func (restriction *Restriction) Lt(col string, val interface{}) *Restriction {
 	return NewRestriction(col, val, "<")
 }
 
-func (restriction Restriction) Like(col string, val interface{}) *Restriction {
+func (restriction *Restriction) Like(col string, val interface{}) *Restriction {
 	return NewRestriction(col, val, "LIKE")
 }
 
-func (restriction Restriction) Between(col string, val1, val2 interface{}) *BetweenRestriction {
+func (restriction *Restriction) Between(col string, val1, val2 interface{}) *BetweenRestriction {
 	return NewBetweenRestriction(col, val1, val2, "BETWEEN")
 }
 
-func (restriction Restriction) In(col string, val []interface{}) *InRestriction {
+func (restriction *Restriction) In(col string, val []interface{}) *InRestriction {
 	return NewInRestriction(col, val, "IN")
 }
 
@@ -127,10 +140,10 @@ func (query *Query) Project(fields ...string) *Query {
 	return query
 }
 
-func (query *Query) Tables(tables ...string) *Query {
+/*func (query *Query) Tables(tables ...string) *Query {
 	query.From = strings.Join(tables, ",")
 	return query
-}
+}*/
 
 func (query *Query) AddCriteria(criteria *Criteria) *Query {
 	query.Filter = criteria
@@ -139,11 +152,12 @@ func (query *Query) AddCriteria(criteria *Criteria) *Query {
 
 func (query *Query) transform() {
 	var tokens []string = make([]string, 0)
+	//var ent string
 	if query.Projection != "" {
 		tokens = append(tokens, "SELECT", query.Projection)
 	}
-	if query.From != "" {
-		tokens = append(tokens, "FROM", query.From)
+	if query.Filter.Entity != "" {
+		tokens = append(tokens, "FROM", reflect.TypeOf(query.Filter.Entity).Elem().Name())
 	}
 	if len(query.Filter.Restrictions) > 0 {
 		//TODO
@@ -153,10 +167,13 @@ func (query *Query) transform() {
 			switch t := query.Filter.Restrictions[i].(type) {
 			case *Restriction:
 				tokens = append(tokens, query.Filter.Restrictions[i].(*Restriction).tostring())
+				tokens = append(tokens, query.Filter.combiners[i])
 			case *BetweenRestriction:
 				tokens = append(tokens, query.Filter.Restrictions[i].(*BetweenRestriction).tostring())
+				tokens = append(tokens, query.Filter.combiners[i])
 			case *InRestriction:
 				tokens = append(tokens, query.Filter.Restrictions[i].(*InRestriction).tostring())
+				/*tokens = append(tokens, query.Filter.combiners[i])*/
 			default:
 				fmt.Printf("%T", t)
 			}
@@ -167,16 +184,21 @@ func (query *Query) transform() {
 }
 
 func main() {
-
-	query := NewQuery()
-	query.Project("name", "age", "rollno", "Student.name").Tables("User", "Student")
-	//query.transform()
-	criteria := CreateCriteria()
-	res := Restriction{}
+	var student Student
+	criteria := CreateCriteria(&student)
+	var restriction Restriction
 	ag := make([]interface{}, 0)
 	ag = append(ag, "10")
 	ag = append(ag, "20")
-	criteria.add(res.Equal("age", "22")).add(res.Gt("rollno", "900")).add(res.NotEqual("age", "80")).add(res.Between("name", "00", "9")).add(res.In("age", ag))
+	criteria.add(restriction.Equal("age", "22")).add(restriction.Gt("rollno", "900")).add(restriction.NotEqual("age", "80")).add(restriction.Between("name", "00", "9")).add(restriction.In("age", ag))
+	query := NewQuery()
+	query.Project("name", "age", "rollno", "Student.name") /*.Tables("User", "Student")*/
 	query.AddCriteria(criteria)
 	query.transform()
+}
+
+type Student struct {
+	name   string
+	age    int
+	rollno int
 }
