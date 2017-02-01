723bc704f43e7366baef7aab444ef132f68f0c5d
bharat <bharat72007@gmail.com>
2017-02-02 00:36:22 +0530

    adding limits and offsets

--- query.go
+++ query.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 	"strings"
 )
 
 type Query struct {
 	Projection string
 	Filter     *Criteria
-	/*From       string*/
+	From       string
 }
 
 type Criteria struct {
@@ -38,6 +39,16 @@ type InRestriction struct {
 	resttype string
 }
 
+type LimitRestriction struct {
+	rowscount int
+	resttype  string
+}
+
+type OffsetRestriction struct {
+	offset   int
+	resttype string
+}
+
 func CreateCriteria(class interface{}) *Criteria {
 	return &Criteria{Restrictions: make([]interface{}, 0), combiners: make([]string, 0), Entity: class}
 }
@@ -46,6 +57,14 @@ func NewBetweenRestriction(col string, lval, rval interface{}, rtype string) *Be
 	return &BetweenRestriction{column: col, lbound: lval, ubound: rval, resttype: rtype}
 }
 
+func NewLimitRestriction(rows int, rtype string) *LimitRestriction {
+	return &LimitRestriction{rowscount: rows, resttype: rtype}
+}
+
+func NewOffsetRestriction(start int, rtype string) *OffsetRestriction {
+
+	return &OffsetRestriction{offset: start, resttype: rtype}
+}
 func NewInRestriction(col string, vals []interface{}, rtype string) *InRestriction {
 	return &InRestriction{column: col, list: vals, resttype: rtype}
 }
@@ -82,6 +101,16 @@ func (criteria *Criteria) addOr(restriction1, restriction2 interface{}) *Criteri
 func (restriction *Restriction) tostring() string {
 
 	strs := restriction.column + " " + restriction.resttype + " " + restriction.value.(string)
+	return strs
+}
+
+func (restriction *LimitRestriction) tostring() string {
+	strs := "LIMIT" + " " + strconv.Itoa(restriction.rowscount)
+	return strs
+}
+
+func (restriction *OffsetRestriction) tostring() string {
+	strs := "OFFSET" + " " + strconv.Itoa(restriction.offset)
 	return strs
 }
 
@@ -135,6 +164,14 @@ func (restriction *Restriction) In(col string, val []interface{}) *InRestriction
 	return NewInRestriction(col, val, "IN")
 }
 
+func (restriction *Restriction) Limit(rows int) *LimitRestriction {
+	return NewLimitRestriction(rows, "LIMIT")
+}
+
+func (restriction *Restriction) Offset(start int) *OffsetRestriction {
+	return NewOffsetRestriction(start, "OFFSET")
+}
+
 func (query *Query) Project(fields ...string) *Query {
 	query.Projection = strings.Join(fields, ",")
 	return query
@@ -148,6 +185,12 @@ func (query *Query) Project(fields ...string) *Query {
 func (query *Query) AddCriteria(criteria *Criteria) *Query {
 	query.Filter = criteria
 	return query
+}
+
+func (criteria *Criteria) List() []interface{} {
+	newquery := Query{Projection: "*", From: reflect.TypeOf(criteria.Entity).Elem().Name()}
+	newquery.transform()
+	return nil
 }
 
 func (query *Query) transform() {
@@ -173,28 +216,39 @@ func (query *Query) transform() {
 				tokens = append(tokens, query.Filter.combiners[i])
 			case *InRestriction:
 				tokens = append(tokens, query.Filter.Restrictions[i].(*InRestriction).tostring())
-				/*tokens = append(tokens, query.Filter.combiners[i])*/
+			/*tokens = append(tokens, query.Filter.combiners[i])*/
+			case *LimitRestriction:
+				tokens = append(tokens, query.Filter.Restrictions[i].(*LimitRestriction).tostring())
+			case *OffsetRestriction:
+				tokens = append(tokens, query.Filter.Restrictions[i].(*OffsetRestriction).tostring())
+
 			default:
 				fmt.Printf("%T", t)
 			}
 		}
 
 	}
+
 	fmt.Println(tokens)
 }
 
 func main() {
-	var student Student
+	var student Student = Student{name: "abc", age: 9, rollno: 100}
 	criteria := CreateCriteria(&student)
 	var restriction Restriction
 	ag := make([]interface{}, 0)
 	ag = append(ag, "10")
 	ag = append(ag, "20")
-	criteria.add(restriction.Equal("age", "22")).add(restriction.Gt("rollno", "900")).add(restriction.NotEqual("age", "80")).add(restriction.Between("name", "00", "9")).add(restriction.In("age", ag))
+	criteria.add(restriction.Equal("age", "22")).add(restriction.Gt("rollno", "900")).add(restriction.NotEqual("age", "80")).add(restriction.Between("name", "00", "9")).add(restriction.In("age", ag)).add(restriction.Limit(10)).add(restriction.Offset(2))
 	query := NewQuery()
 	query.Project("name", "age", "rollno", "Student.name") /*.Tables("User", "Student")*/
 	query.AddCriteria(criteria)
 	query.transform()
+
+	//Another Example
+	//criteria2 := CreateCriteria(&student)
+	//This will return the list of all records. criteria2.List() ==>SELECT * FROM Student
+
 }
 
 type Student struct {
